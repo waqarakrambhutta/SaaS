b977d8727074e09870bd3521ae2e939822507689
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-11-23 03:59:32 +0500

    format the html

--- src/home/views.py
+++ src/home/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 html_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-    print(html_dir)
+    page_title = 'My page will be'
+    my_content = {
+        "title": page_title,
+    }
     html_ = """
     <!DOCTYPE html>
     <html lang="en">
@@ -14,21 +17,10 @@ def home_page_view(request, *args, **kwargs):
         <title>Document</title>
     </head>
     <body>
-        <h1>This is any<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <h1>This is Anything!</h1>
-  </body>
-</html>
-thing!</h1>
+        <h1>{title} anything</h1>
     </body>
     </html>
-
-    """
+    """.format(**my_content)
     # html_file_path = html_dir / 'home.html'
     # html_= html_file_path.read_text() 
     return HttpResponse(html_)
