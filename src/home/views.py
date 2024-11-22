e60dee46ba794b03ab4960e739f15670a339c5ed
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-11-23 03:46:45 +0500

    Render Html file

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,11 @@
+import pathlib
 from django.http import HttpResponse
 
+html_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h2>Hello World!</h2>")
+    print(html_dir)
+    html_ = ""
+    html_file_path = html_dir / 'home.html'
+    html_= html_file_path.read_text() 
+    return HttpResponse(html_)
