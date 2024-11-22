00c4634122e851625b7f1b15fd336b6b8cb676ab
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-11-23 03:49:04 +0500

    Render Html file

--- src/home/views.py
+++ src/home/views.py
@@ -5,7 +5,20 @@ html_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
     print(html_dir)
-    html_ = ""
-    html_file_path = html_dir / 'home.html'
-    html_= html_file_path.read_text() 
+    html_ = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>This is anything!</h1>
+    </body>
+    </html>
+
+    """
+    # html_file_path = html_dir / 'home.html'
+    # html_= html_file_path.read_text() 
     return HttpResponse(html_)
