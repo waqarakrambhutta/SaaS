fce46cff03c22d2f258ebacb57418f6421563c01
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-11-27 04:16:17 +0500

    Hard way to Manage environment variables

--- src/home/settings.py
+++ src/home/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t70wp#)yg$&hl44$ef@v_0xu9c!+%u6$xf$)#x2&n-fdan-yeq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true' or True
+
+print(type(DEBUG))
 
 ALLOWED_HOSTS = [
     '.railway.app', # https://saas.prod.railway.app
