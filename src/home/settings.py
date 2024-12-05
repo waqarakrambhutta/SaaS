15c7c2d11eb57a66e6bb4be24e1f3d9e47b90922
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-12-05 17:46:15 +0500

    Neon database connection settings done

--- src/home/settings.py
+++ src/home/settings.py
@@ -93,6 +93,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
+DATABASE_URL = config('DATABASE_URL', cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url 
+    DATABASES = {
+    'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
