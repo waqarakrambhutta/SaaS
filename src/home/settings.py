9bc6503d84bae4002d73ed82b9603630f74c4d98
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2024-12-06 16:25:17 +0500

    Prepare static files

--- src/home/settings.py
+++ src/home/settings.py
@@ -142,6 +142,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+STATIC_ROOT = BASE_DIR.parent / 'local-cdn'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
