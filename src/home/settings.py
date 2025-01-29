3d3f062e9e92a1bfbf316ad43082173a2e933101
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2025-01-29 18:46:52 +0500

    Minor chnages

--- src/home/settings.py
+++ src/home/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
