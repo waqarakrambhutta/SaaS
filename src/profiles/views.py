6d7502911d40cfdd92d577c05951a87fd4e3dea0
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2025-02-07 22:22:22 +0500

    Minor changes

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 @login_required
 def profile_view(request,username=None,*args, **kwargs):
     user = request.user
