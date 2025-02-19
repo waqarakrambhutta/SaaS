e2355d1953018f7aa3e4f85b0431aaf5fc3543ce
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-02-19 16:13:51 +0500

    Minor changes

--- src/home/settings.py
+++ src/home/settings.py
@@ -17,6 +17,9 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
