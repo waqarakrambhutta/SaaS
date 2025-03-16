88257a4abfcecc1c56cefb98b3160dec93531f37
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-03-16 15:47:24 +0500

    Permission of the user and handling them in the templates

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import profile_view
+from .views import profile_detail_view, profile_list_view
 
 urlpatterns = [
-    path("<username>/",profile_view),  
+    path("<username>/",profile_detail_view),  
+    path("",profile_list_view)
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -7,9 +7,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @login_required
-def profile_view(request,username=None,*args, **kwargs):
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    return render(request, "profiles/list.html", context)
+
+@login_required
+def profile_detail_view(request,username=None,*args, **kwargs):
     user = request.user
-    # profile_user_object = User.objects.get(username=username)
     profile_user_object=get_object_or_404(User, username=username)
     is_me = profile_user_object == user
-    return HttpResponse(f'Hello there {username} - {profile_user_object.id} - {user.id} - {is_me}')
+    context = {
+        "object":profile_user_object,
+        "instance":profile_user_object,
+        "owner": is_me
+    }
+    return render(request, "profiles/detail.html", context)
