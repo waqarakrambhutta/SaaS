b1368f54599bee0360bf1a1f297af3b078c582ed
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-03-21 10:17:16 +0500

    Minor changes

--- src/home/settings.py
+++ src/home/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
