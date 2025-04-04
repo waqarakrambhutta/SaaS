fdef82e86bc6f90048cc232a512b878a77c68db2
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-04-04 20:33:52 +0500

    Groups and permission in the Sibscription model

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-class Subscription(models.Model):
-    name = models.CharField(max_length=120)
-
-    class Meta:
-        permissions = [
+SUBSCRIPTIONS_PERMISSIONS = [
             ("advance", "Advanced Pro Plan"), # subscriptions.advance
             ("pro", "Pro Perm"), # subscriptions.pro
             ("basic", "Basic Perm"), # subscriptions.basic
             ("basic_ai", "Basic AI Perm")
         ]
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=120)
+    group = models.ManyToManyField(Group)
+    permission = models.ManyToManyField(Permission, limit_choices_to={
+        'content_type__app_label':'subscriptions',
+        'codename__in': [x[0] for x in SUBSCRIPTIONS_PERMISSIONS]
+    })
+
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        permissions = SUBSCRIPTIONS_PERMISSIONS
