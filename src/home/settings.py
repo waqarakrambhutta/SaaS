617ca8f6461c48234cf72ab594b7651089b143c2
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-04-06 13:19:11 +0500

    Minor changes

--- src/home/settings.py
+++ src/home/settings.py
@@ -29,8 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true' or True
 
